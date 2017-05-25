3fd5aff79c28d1c46c046a1fc65b5dc84217a23e
iurii <iurii.derevianko@gmail.com>
2017-05-25 12:51:39 +0300

    Test commit.

--- lesson-01/task-03-Matrix/src/ua/itea/javabasic/practice/lesson01/matrix/Main.java
+++ lesson-01/task-03-Matrix/src/ua/itea/javabasic/practice/lesson01/matrix/Main.java
@@ -3,7 +3,6 @@ package ua.itea.javabasic.practice.lesson01.matrix;
 public class Main {
     public static void main(String[] args) {
         System.out.println("The Matrix has you...");
-        System.out.println("The Matrix has you...");
     }
 
 }
